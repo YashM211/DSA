bc6e945e403be2fc9d78ee5938ad1a3ba6d7c267
Yash Mohta <101795345+YashM211@users.noreply.github.com>
2024-07-09 19:31:52 +0530

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -1,120 +1,131 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
+//Build Suffix Array
+#include<bits/stdc++.h>
 using namespace std;
-
-// } Driver Code Ends
-// User function template for C++
-#define ll long long int
-
-ll quickhash(string s, ll p, ll mod)
-{
-    ll ans = (s[0] - 'a') + 1;
-    for (int i = 1; i < s.size(); i++)
-    {
-        ans = ((ans * p + s[i] - 'a' + 1) % mod + mod) % mod;
-    }
-    return ans;
-}
-
-class hassh
-{
-private:
-    vector<ll> phash;
-    vector<ll> pk;
-    ll mod;
-    ll n;
-    ll p;
-
-public:
-    hassh(string s, ll mmod, ll pp)
-    {
-        n = (ll)s.size();
-        mod = mmod;
-        p = pp;
-        phash.resize(n);
-        pk.resize(n);
-    }
-
-    void hasher(string s)
-    {
-        phash[0] = s[0] - 'a' + 1ll;
-        pk[0] = 1;
-        for (int i = 1; i < n; i++)
-        {
-            phash[i] = ((phash[i - 1] * p + s[i] - 'a' + 1) % mod + mod) % mod;
-            pk[i] = ((pk[i - 1] * p) % mod + mod) % mod;
+ 
+struct hasher{
+    int n, _k, _p;
+    vector<int> powk,base,rev;
+    
+    void init(string s,int k,int p){
+        _k = k, _p = p;
+        n = s.length();
+        powk.resize(n+1);
+        base.resize(n+1);
+        
+        // precompute
+        base[0] = 0;
+        powk[0] = 1;
+        for(int i=0;i<n;i++){
+            int num = (s[i]-'a'+1 )%p;
+            base[i+1] = (1LL *base[i] * k + num)%p;
+            powk[i+1] = (1LL* k * powk[i]) %p;
         }
+        
+        // reverse
+        // rev.resize(n+1);
+        // rev[n] = 0;
+        // for(int i=n-1;i>=0;i--){
+        //     int num = (s[i]-'a'+1 )%p;
+        //     rev[i] = (1LL *rev[i+1] * k + num)%p;
+        // }
+    }  
+    int gethash(int l,int r){ // 0 index
+        int ans = (base[r+1] - 1LL *base[l]*powk[(r-l+1)])%_p;
+        return (ans+_p)%_p;
     }
-
-    ll gethash(int l, int r)
-    {
-        if (l == 0)
-            return (phash[r]) % mod;
-        else
-            return ((phash[r] - phash[l - 1] * pk[r - l + 1]) % mod + mod) % mod;
-    }
-
-    ll getphash(int i)
-    {
-        return phash[i];
+    // reverse hash
+    int getrevhash(int l,int r){
+        int ans = (rev[l] - 1LL *rev[r+1]*powk[(r-l+1)])%_p;
+        return (ans+_p)%_p;
     }
 };
-
-class Solution
-{
-public:
-    int search(string text, string pat)
-    {
-        // Your code goes here
-        ll mod = 1e9 + 7;
-        ll p = 31;
-        ll pathashval = quickhash(pat, p, mod);
-        // return pathashval;
-
-        int wsize = pat.size();
-        hassh texthash(text, mod, p);
-        texthash.hasher(text);
-        // cout<<pathashval<<endl;
-        // cout<<texthash.gethash(8,9)<<endl;
-
-        // for(int i=0 ; i<text.size() ; i++){
-        //     cout<<texthash.getphash(i)<<endl;
-        // }
-
-        // cout<<texthash.getphash(9)<<" "<<texthash.getphash(8)<<" "<<texthash.getphash(7)<<endl;
-        for (int i = 0; i + pat.size() - 1 < text.size(); i++)
-        {
-            // cout<<i<<" ";
-            ll res = texthash.gethash(i, i + wsize - 1);
-            if (res == pathashval)
-            {
-                return true;
+ 
+ 
+struct double_hash{
+    string temp;
+    hasher a,b;
+    void init(string s){
+        temp = s;
+        a.init(s,37,1000000021);
+        b.init(s,39,1000000009);
+    }
+    pair<int,int> gethash(int l,int r){
+        return {a.gethash(l,r),b.gethash(l,r)};
+    }
+    pair<int,int> getrevhash(int l,int r){
+        return {a.getrevhash(l,r),b.getrevhash(l,r)};
+    }
+    bool is_palin(int l,int r){
+        return gethash(l,r) == getrevhash(l,r);
+    }
+    int lex_order(int i,int j,int len){ // O(log(len))
+        int ans = 0;
+        int lo = 1;
+        int hi = len;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(gethash(i,i+mid-1)==gethash(j,j+mid-1)){
+                ans = mid;
+                lo = mid+1;
+            }else{
+                hi = mid-1;
             }
         }
-        return false;
+        if(ans == len){
+            return 0;
+        }else{
+            if(temp[i+ans]<temp[j+ans]){
+                return -1; 
+            }else{
+                return 1;
+            }
+        }
     }
 };
-
-//{ Driver Code Starts.
-
+ 
+ 
+int n;
+double_hash ha;
+bool cmp(int i,int j){
+    int len = n-max(i,j);
+    int oval = ha.lex_order(i,j,len);
+    if(oval==0){
+        if(i>j){
+            return true;
+        }else{
+            return false;
+        }
+    }else{
+        if(oval<0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+ 
+vector<int> get_suffix_array(string s){
+    n = s.length();
+    ha.init(s);
+    
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        arr[i]=i;
+    }
+    sort(arr.begin(),arr.end(),cmp);
+    return arr;
+}
+ 
+void solve(){
+    string s;
+    cin>>s;
+    for(auto v:get_suffix_array(s)){
+        cout<<v<<" ";
+    }
+}
+ 
 int main()
 {
-
-    ios_base::sync_with_stdio(0);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        string x, y;
-        cin >> x >> y;
-
-        Solution ob;
-        cout << ob.search(x, y) << "\n";
-    }
-
+    solve();
     return 0;
 }
-// } Driver Code Ends
